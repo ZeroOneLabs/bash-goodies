e73d861e78fbbb92eb793352d17bf5c2e83ec394
Zan <75501500+ZeroOneLabs@users.noreply.github.com>
2023-02-17 22:33:01 -0700

    Updated some syntax and naming on aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -1,24 +1,38 @@
 alias serve="python -m SimpleHTTPServer"
 
-#o alias will only work on Mac OS
-alias o="open"
-
 # Find process with open port
-popFn() {
+port_open_function() {
+    '''
+    port_open_function() -  Will return a command, PID, User... Node, etc.
+                            Will return nothing if the port is not open.
+    '''
+    if [ ! -n "$1" ];then 
+        echo "pop requires one integer argument"
+        return 1
+    fi
     lsof -i tcp:$1
 }
-alias pop=popFn
+alias pop=port_open_function
 
 #Source - http://apple.stackexchange.com/a/5461
-pmanFn() {
-    man -t "${1}" | open -f -a /Applications/Preview.app
+preview_man() {
+    # Preview now lives in /System/Applications
+    man -t "${1}" | open -f -a /System/Applications/Preview.app
 }
-alias pman=pmanFn
+# An extension of 'man' should start with 'man'.
+alias manpdf=preview_man
 
 #Source - http://apple.stackexchange.com/a/5514
 alias ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'"
-alias myip="dig +short myip.opendns.com @resolver1.opendns.com"
+
+# IP addresses can change over time. Let's "wrap" this in a function!
+get_my_ip() {
+    myip="dig +short myip.opendns.com @resolver1.opendns.com"
+    echo "${myip}"
+}
+alias myip=get_my_ip
 
 #Source - http://apple.stackexchange.com/a/118736
 #On Mac, copy the current path to the clipboard
-alias path='echo | pwd | pbcopy'
+# - Changing to pbpath because 'path' is too ambiguous.
+alias pbpath='echo | pwd | pbcopy'
